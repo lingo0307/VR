af59f35e0a8a377f78994054f1ac4c4bf06f9e4e
lingo0307 <0902260710l@gmail.com>
2025-04-25 10:45:52 +0800

    1

---
+++ home/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.7293517392230804,
+        "pitch": 0.035012288801484104,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.5163941655137894,
+          "pitch": 0.17158749387807148,
+          "title": "九份老街入口",
+          "text": "<br>"
+        }
+      ]
+    }
+  ],
+  "name": "九份老街",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
